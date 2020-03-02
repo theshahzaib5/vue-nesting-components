e314eb51763192208c782c2a78838d86cca527f8
Shah Zaib <shah.zaib@kwanso.com>
2020-03-02 18:31:32 +0500

    added some router links to header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,31 @@
 <template>
   <header class="text-center">
-    <h1 @click="changeTitle">{{ title }}</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" @click.prevent="changeTitle" href="/">{{ title }}</a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link" exact>Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/addBlog" class="nav-link" exact>Add Blog</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
 </template>
 
@@ -40,6 +65,5 @@ export default {
 
 header {
   background: $GREY;
-  padding: 15px 15px;
 }
 </style>
